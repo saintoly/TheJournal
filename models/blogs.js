1bc9795fe3ed03ae72e7c87e1548098b52eb2158
Ola Joseph <joseph4oly@gmail.com>
2017-07-06 16:20:38 +0200

    work

--- models/blogs.js
+++ models/blogs.js
@@ -3,19 +3,3 @@
 //content. type string
 //date, type date
 //author, type string
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-mongoose.connect('mongodb://localhost/TheJournal',{ useMongoClient: true
-});
-
-const articlesSchema = new Schema({
-  title: String,
-  content: String,
-  date: Date,
-  author: String
-})
-
-const article = mongoose.model('article', articlesSchema)
-
-
-module.exports = article
